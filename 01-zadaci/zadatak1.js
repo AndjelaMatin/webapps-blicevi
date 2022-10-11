2ccb585970383c28e86a825c803e11fae5dc470b
Anđela Matin <andjelamatin@gmail.com>
2022-10-11 08:22:20 +0200

    Rjesen Zadatak 1

---
+++ 01-zadaci/zadatak1.js
@@ -0,0 +1,9 @@
+a = [{ 1: "A" }, { 1: "C", 2: "A" }, { 2: "C" }];
+function vrati(a) {
+  var lista = [];
+  for (var i of a) 
+    if (a[i].Object.keys(object).length == 1) 
+        lista.push(a[i]);
+return lista;
+}
+console.log(vrati(a));
